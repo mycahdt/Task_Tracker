c387d7efa559377b223929aef20564a6073e2947
mycahdt <mycahdt@udel.edu>
2023-05-04 19:34:40 -0400

    Updates

--- src/interfaces/publish.ts
+++
@@ -1,5 +0,0 @@
-/* 3.28 A representation of whether a question is published or unpublished*/
-export interface Publish {
-    /** Whether or not this question is ready to display to students */
-    published: boolean;
-}
